7ce7ba69009be94706dd95a96aa258cbaacb8f1b
Santiago Toledo <santiago.toledo@teamcmp.com>
2022-03-15 17:13:13 +0100

    Fix current db clearing on backup

--- scripts/chia--restore.sh
+++ scripts/chia--restore.sh
@@ -7,8 +7,7 @@ NC=$'\e[0m' # No Color
 DOCKER_COMPOSE="docker-compose -f ${HOME}/chia/docker-compose.yml -f ${HOME}/chia/docker-compose.override.yml"
 
 DB_BACKUP_FILE="${HOME}/chia/disks/chia-fd-1/blockchain_v2_mainnet.sqlite"
-DB_CURRENT_FOLDER="${HOME}/chia/.chia/mainnet/db"
-DB_CURRENT_FILE="${DB_CURRENT_FOLDER}/blockchain_v2_mainnet.sqlite"
+DB_CURRENT_FILE="${HOME}/chia/.chia/mainnet/db/blockchain_v2_mainnet.sqlite"
 
 if ! test -f "${DB_BACKUP_FILE}"; then
   echo "$(date) | ${RED}Whoops! Looks like ${DB_BACKUP_FILE} doesn't exist${NC}"
@@ -20,8 +19,8 @@ echo "$(date) | ${GREEN}Starting restore${NC}"
 echo "$(date) | ${GREEN}-> Stopping chia${NC}"
 ${DOCKER_COMPOSE} stop chia
 
-echo "$(date) | ${GREEN}-> Clearing db folder${NC}"
-rm "${DB_CURRENT_FOLDER}/*"
+echo "$(date) | ${GREEN}-> Clearing current db${NC}"
+rm -f "${DB_CURRENT_FILE}*"
 
 echo "$(date) | ${GREEN}-> Restoring backup from ${DB_BACKUP_FILE} to ${DB_CURRENT_FILE}${NC}"
 cp "${DB_BACKUP_FILE}" "${DB_CURRENT_FILE}"
